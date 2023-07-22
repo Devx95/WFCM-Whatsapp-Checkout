b37e4b6ece6e6ed6b07712ae30d32baa82efa6b8
Devx95 <116139473+Devx95@users.noreply.github.com>
2023-07-22 03:04:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -160,15 +160,17 @@ function wfcm_wa_thankyou($title, $order) {
     	$msg .="*Info del comprador*: \n";
 
 	$msg .="Name: ".$order->{"get_".$mode."_first_name"}()." ".$order->{"get_".$mode."_last_name"}()."\n";
-	if($mode=='billing'){
-		$email = (isset($order->shipping['email']))?$order->shipping['email']:$order->get_billing_email();
+    	$msg .="Address: ".implode(', ',[$order->{"get_".$mode."_address_1"}(),$order->{"get_".$mode."_address_2"}()])."\n";
+    	$msg .="City: ".$order->{"get_".$mode."_city"}().", ".$province.", ".$country."\n";
+    	$msg .="Zip Code: ".$order->{"get_".$mode."_postcode"}()."\n";
+    	if($mode=='billing'){
+    		$email = (isset($order->shipping['email']))?$order->shipping['email']:$order->get_billing_email();
     		$phone = (isset($order->shipping['phone']))?$order->shipping['phone']:$order->get_billing_phone();
     	}else{
-		$name = $order->get_billing_name();
-		$email = $order->get_billing_email();
+    		$email = $order->get_billing_email();
     		$phone = $order->get_billing_phone();
     	}
-
+		
 	$msg .="Name: ".$name."\n";
 	$msg .="Email: ".$email."\n";
     	$msg .="Phone Number: ".$phone."\n";
