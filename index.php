b3162ec324f476babd54679fdb9b1e9321284e1c
Devx95 <116139473+Devx95@users.noreply.github.com>
2023-07-22 02:54:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -160,7 +160,6 @@ function wfcm_wa_thankyou($title, $order) {
     	$msg .="*Info del comprador*: \n";
 	$msg .="Name: ".$order->{"get_".$mode."_first_name"}()."\n";	
     	if($mode=='shipping'){
-		$name = (isset($order->shipping['name']))?$order->shipping['name']:$order->get_billing_first_name();
 		$email = (isset($order->shipping['email']))?$order->shipping['email']:$order->get_billing_email();
     		$phone = (isset($order->shipping['phone']))?$order->shipping['phone']:$order->get_billing_phone();
     	}else{
